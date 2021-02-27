a06e1e11f7ad749c8fd105fccb2e42d290f919c7
devmdeniz <mehmetdeniz1397@gmail.com>
2021-02-28 00:26:14 +0300

    Update update.php

--- system/update.php
+++ system/update.php
@@ -1,1 +1,25 @@
-<?php?>
+<?php
+        include("./system/connection.php");
+
+		$lastvisitornumber = $conn -> query("SELECT * FROM visitors ORDER BY visitors DESC LIMIT 1");
+		$outputs = $lastvisitornumber->fetch_array();
+
+		echo $outputs[0];
+
+        $visitor = $_POST["changeparagraph"];
+        $id = $_POST["changeid"];
+
+
+        $sql = "UPDATE visitors  SET visitornumber = '$visitor' WHERE id = '$id' ";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Succesfull UPDATE";
+        header("Location:../../index.php");
+    } 
+    else {
+        echo "ERROR: " . $conn->error;
+    }
+
+
+
+?>
