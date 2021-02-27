3de3fc61e1ff6a2188f9562ec3fefb69824b2721
devmdeniz <mehmetdeniz1397@gmail.com>
2021-02-27 16:57:03 +0300

    Update connection.php

--- system/connection.php
+++ system/connection.php
@@ -1,1 +1,14 @@
-<?php?>
+<?php
+
+$servername = "localhost";
+$database = "showvisitorwebsite";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+	if(!$conn) {
+		die("Failed" . mysqli_connect_error());
+	}
+
+?>
